3ddde08a7eb6b214259653e8c1225cb166990f6e
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-07-14 14:43:34 -0300

    test...

--- js/conection.js
+++ js/conection.js
@@ -1,13 +1,13 @@
 // conection.js
 
 async function productsList() {
-    const connection = await fetch("https://alura-geek-khaki.vercel.app/produtos");
+    const connection = await fetch("https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos");
     const productList = await connection.json();
     return productList;
 }
 
 async function createProduct(title, price, image) {
-    const connection = await fetch("https://alura-geek-khaki.vercel.app/produtos", {
+    const connection = await fetch("https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -24,7 +24,7 @@ async function createProduct(title, price, image) {
 }
 
 async function deleteProduct(productId) {
-    const connection = await fetch(`https://alura-geek-khaki.vercel.app/produtos/${productId}`, {
+    const connection = await fetch(`https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos/${productId}`, {
         method: 'DELETE',
         headers: {
             "Content-Type": "application/json"
