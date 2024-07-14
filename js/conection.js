38b27e830756d7d843ce47ee7c0330e8732d319b
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-07-14 15:57:26 -0300

    test

--- js/conection.js
+++ js/conection.js
@@ -8,28 +8,38 @@ async function productsList() {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const productList = await response.json();
-        return productList;
+        return productList.produtos; // Certifique-se de retornar apenas a lista de produtos
     } catch (error) {
         console.error('Erro ao obter a lista de produtos:', error);
         throw error; // ou trate o erro de outra forma, conforme necessário
     }
 }
 
-async function createProduct(title, price, image) {
-    const connection = await fetch(`${BASE_URL}/produtos`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            title: title,
-            price: price,
-            image: image
-        })
-    });
 
-    const createdProduct = await connection.json();
-    return createdProduct;
+async function createProduct(title, price, image) {
+    try {
+        const response = await fetch("https://alura-geek-nine-psi.vercel.app/produtos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                price: price,
+                image: image
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const createdProduct = await response.json();
+        return createdProduct;
+    } catch (error) {
+        console.error('Erro ao criar produto:', error);
+        throw error; // ou trate o erro de outra forma, conforme necessário
+    }
 }
 
 async function deleteProduct(productId) {
