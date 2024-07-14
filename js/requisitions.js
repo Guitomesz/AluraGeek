148cdd2826ee6e87ef0c167539c2f7d80dd8b15c
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-07-14 14:46:42 -0300

    error...

--- js/requisitions.js
+++ js/requisitions.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
-axios.get('http://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos')
+axios.get('https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos')
     .then(resposta => console.log(resposta.data))
 
-axios.get('http://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos')
+axios.get('https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos')
     .then(resposta => console.log(resposta.data))
 
-axios.post('http://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos', {
+axios.post('https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos', {
     nome: 'Novo projeto incrivel'
 })
     .then(resposta => console.log(resposta.data))
 
-axios.put('http://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos', {
+axios.put('https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos', {
     id: 1,
     nome: 'Novo projeto incrivel'
 })
     .then(resposta => console.log(resposta.data))
 
-axios.delete('http://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos')
+axios.delete('https://alura-geek-4a8pbp2wb-guitomeszs-projects.vercel.app/produtos')
     .then(resposta => console.log(resposta.data))
