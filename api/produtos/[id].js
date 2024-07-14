0070b25d7e3b2e95e8b2b797cdb185dbda26a79c
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-07-14 17:59:22 -0300

    asdsfvzdvxcvcxb

--- api/produtos/[id].js
+++ api/produtos/[id].js
@@ -7,7 +7,6 @@ export default function handler(req, res) {
     const { method } = req;
     const { id } = req.query;
 
-    // Leitura do arquivo JSON
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             res.status(500).json({ error: 'Failed to read data' });
@@ -29,28 +28,6 @@ export default function handler(req, res) {
         }
 
         switch (method) {
-            case 'GET':
-                res.status(200).json(jsonData.produtos[produtoIndex]);
-                break;
-
-            case 'PUT':
-                let body = '';
-                req.on('data', chunk => {
-                    body += chunk.toString();
-                });
-                req.on('end', () => {
-                    const updatedProduto = JSON.parse(body);
-                    jsonData.produtos[produtoIndex] = updatedProduto;
-                    fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), writeErr => {
-                        if (writeErr) {
-                            res.status(500).json({ error: 'Failed to write data' });
-                            return;
-                        }
-                        res.status(200).json(updatedProduto);
-                    });
-                });
-                break;
-
             case 'DELETE':
                 jsonData.produtos.splice(produtoIndex, 1);
                 fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), writeErr => {
@@ -63,7 +40,7 @@ export default function handler(req, res) {
                 break;
 
             default:
-                res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
+                res.setHeader('Allow', ['DELETE']);
                 res.status(405).end(`Method ${method} Not Allowed`);
         }
     });
