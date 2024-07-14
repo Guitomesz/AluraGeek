002608bf634203263a7d1b1954c8dfd6b02f3f63
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-07-14 16:31:25 -0300

    test

--- js/requisitions.js
+++ js/requisitions.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
-axios.get('https://alura-geek-nine-psi.vercel.app/produtos')
+axios.get('https://alura-geek-nine-psi.vercel.app/api/produtos')
     .then(resposta => console.log(resposta.data))
 
-axios.get('https://alura-geek-nine-psi.vercel.app/produtos')
+axios.get('https://alura-geek-nine-psi.vercel.app/api/produtos')
     .then(resposta => console.log(resposta.data))
 
-axios.post('https://alura-geek-nine-psi.vercel.app/produtos', {
+axios.post('https://alura-geek-nine-psi.vercel.app/api/produtos', {
     nome: 'Novo projeto incrivel'
 })
     .then(resposta => console.log(resposta.data))
 
-axios.put('https://alura-geek-nine-psi.vercel.app/produtos', {
+axios.put('https://alura-geek-nine-psi.vercel.app/api/produtos', {
     id: 1,
     nome: 'Novo projeto incrivel'
 })
     .then(resposta => console.log(resposta.data))
 
-axios.delete('https://alura-geek-nine-psi.vercel.app/produtos')
+axios.delete('https://alura-geek-nine-psi.vercel.app/api/produtos')
     .then(resposta => console.log(resposta.data))
