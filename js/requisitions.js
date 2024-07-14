ce4962906ce51fd851bc0378b14cbe12e3538691
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2024-07-14 14:36:22 -0300

    testing vercel requisitions

--- js/requisitions.js
+++ js/requisitions.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
-axios.get('http://localhost:3000/produtos')
+axios.get('https://alura-geek-khaki.vercel.app/produtos')
     .then(resposta => console.log(resposta.data))
 
-axios.get('http://localhost:3000/produtos/1')
+axios.get('https://alura-geek-khaki.vercel.app/produtos/1')
     .then(resposta => console.log(resposta.data))
 
-axios.post('http://localhost:3000/produtos', {
+axios.post('https://alura-geek-khaki.vercel.app/produtos', {
     nome: 'Novo projeto incrivel'
 })
     .then(resposta => console.log(resposta.data))
 
-axios.put('http://localhost:3000/produtos/1', {
+axios.put('https://alura-geek-khaki.vercel.app/produtos/1', {
     id: 1,
     nome: 'Novo projeto incrivel'
 })
     .then(resposta => console.log(resposta.data))
 
-axios.delete('http://localhost:3000/produtos/1')
+axios.delete('https://alura-geek-khaki.vercel.app/produtos/1')
     .then(resposta => console.log(resposta.data))
